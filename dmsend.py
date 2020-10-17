ad6fce3d3d15e2c6d06b6f4dbcc1f5a9cfe8e603
soda87 <73013824+soda87@users.noreply.github.com>
2020-10-17 17:01:20 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -11,7 +11,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('코스모:분석중')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -27,11 +27,11 @@ async def on_message(message):
                     #메시지 관리권한이 있을시 사용가능
                     if message.author.guild_permissions.manage_messages:
                         embed = discord.Embed(color=0x1DDB16, timestamp=message.created_at)
-                        embed.add_field(name="★★제목★★", value=msg, inline=True)
-                        embed.set_footer(text="★맨 밑에 들어갈 내용★")
+                        embed.add_field(name="코스모 : 분석방", value=msg, inline=True)
+                        embed.set_footer(text="필자 : 공지")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('NzY2NjMyODQzMjQ5MzE5OTM2.X4mMeQ.DRngyKCFB_jpRpI3fYfV9KbC5Ak')
